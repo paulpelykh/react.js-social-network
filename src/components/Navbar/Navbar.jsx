10ab16dbc3431992472c3faaa7fe0c5d1fc59fab
Paul Pelykh <eeddll1505@gmail.com>
2020-04-14 12:54:29 +0300

    Used Class Component in Users

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,9 +12,13 @@ const Navbar = (props) => {
 	        <div className={s.item}>
 	          <NavLink to="/profile" activeClassName={s.activeLink}>Profile</NavLink>
 	        </div>
-	        <div className={`{s.item} {s.active}`}>
+	        <div className={`${s.item} ${s.active}`}>
 	          <NavLink to="/dialogs" activeClassName={s.activeLink}>Messages</NavLink>
 	        </div>
+	        <div className={`${s.item} ${s.active}`}>
+	          <NavLink to="/users" activeClassName={s.activeLink}>Users</NavLink>
+	        </div>
+
 	        <div className={s.item}>
 	          <NavLink to="/news" activeClassName={s.activeLink}>News</NavLink>
 	        </div>
