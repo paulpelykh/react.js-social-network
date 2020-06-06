d001a17cd5e0d80c6269b6a32b5a7f4c00e2a854
paulpelykh <eeddll1505@gmail.com>
2020-06-06 15:57:07 +0300

    Added login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import News from './components/News/News';
@@ -10,11 +9,12 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
+import HeaderContainer from './components/Header/HeaderContainer';
 
 const App = (props) => {
   return (
     <div className='app-wrapper'>
-      <Header />
+      <HeaderContainer />
       <Navbar store={props.store} />
       <div className='app-wrapper-content'>
         <Route path="/dialogs"
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import s from './Header.module.css';
+import { NavLink } from 'react-router-dom';
 
-const Header = () => {
+const Header = (props) => {
 	return (
 		<header className={s.header}>
-    	    <img src="https://upload.wikimedia.org/wikipedia/commons/a/ab/Android_O_Preview_Logo.png" alt=""/>
-     	 </header>
+			<img src="https://upload.wikimedia.org/wikipedia/commons/a/ab/Android_O_Preview_Logo.png" alt="" />
+
+			<div className={s.loginBlock}>
+				{props.isAuth ? props.login
+					: <NavLink to={'/login'}>login</NavLink>}
+			</div>
+		</header>
 	);
 }
 
